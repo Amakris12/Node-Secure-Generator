cddea29c8beba211c6e11a1e0b95818da71e42b7
Amakris12 <amakri386@west-mec.org>
2022-05-06 14:04:21 -0700

    push

--- public/scripts/holder.js
+++
@@ -1,4 +0,0 @@
-function companyName(){
-    document.getElementById('company').value = document.getElementById('savedCompany').value;
-    document.getElementById('password').value = document.getElementById('savedPassword').value;
- }
